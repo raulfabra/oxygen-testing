c5007fe76b6e2681686120d0465a80da8cb3af8e
Raúl Fabra <fabraraul@yahoo.com>
2024-09-19 09:16:45 +0200

    Finished implement class Booking, without funcionalities

--- index.js
+++ index.js
@@ -6,10 +6,34 @@ class Room {
     this.discount = discount;
   }
 
-  isOccupied(date) {
-    for (const booking of this.bookings) {
-    }
+  static totalOccupancyPercentage(rooms, startDate, endDate) {
+    // Devuelve el Percentage de Ocupación TOTAL de todo el hotel dentro del rango pasado por parámetros, incluidos los extremos
+  }
+  static availableRooms(rooms, startDate, endDate) {
+    // Devuelve las habitaciones que no estan ocupadas en el rango pasado por parametros, incluidos los extremos. === HABITACIONES LIBRES ENTRE EL RANGO PASADO
   }
 
-  occupancyPercentage(startDate, endDate) {}
+  isOccupied(date) {
+    // Devuelve Falso si la hab. no esta ocupada = DISPONIBLE
+    // Devuelve True si la hab. esta ocupada = RESERVADA
+  }
+
+  occupancyPercentage(startDate, endDate) {
+    // Devuelve el Porcentage de ocupación dentro del rango pasado por parámetros, incluidos los extremos
+  }
+}
+
+class Booking {
+  constructor(name, email, checkIn, checkOut, discount, room) {
+    this.name = name;
+    this.email = email;
+    this.checkIn = checkIn;
+    this.checkOut = checkOut;
+    this.discount = discount;
+    this.room = room;
+  }
+
+  fee() {
+    // Devuelve el precio final de la reserva contando el descuento de la habitacion (Hotel) y de la reserva (Pagina de reserva)
+  }
 }
