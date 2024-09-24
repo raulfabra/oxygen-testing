386a8ea72c169117cf61477bb201c2b20c00d0e5
Raúl Fabra <fabraraul@yahoo.com>
2024-09-24 09:28:13 +0200

    First Transform the Javascript to Typescript with config.

--- index.ts
+++ index.ts
@@ -1,12 +1,24 @@
+interface RoomInterface {
+  name: string;
+  bookings: Booking[];
+  price: number;
+  discount: number;
+}
+
 class Room {
-  constructor(name, bookings, rate, discount) {
+  name: string;
+  bookings: Booking[];
+  price: number;
+  discount: number;
+
+  constructor({ name, bookings, price, discount }: RoomInterface) {
     this.name = name;
     this.bookings = bookings;
-    this.rate = rate;
+    this.price = price;
     this.discount = discount;
   }
 
-  static totalOccupancyPercentage(rooms, startDate, endDate) {
+  static totalOccupancyPercentage(rooms: Room[], startDate: string, endDate: string) {
     // Devuelve el Percentage de Ocupación TOTAL de todo el hotel dentro del rango pasado por parámetros, incluidos los extremos
     let percentageSum = 0;
 
@@ -14,9 +26,9 @@ class Room {
       percentageSum += room.occupancyPercentage(startDate, endDate);
     }
 
-    return percentageSum / roomList.length;
+    return percentageSum / rooms.length;
   }
-  static availableRooms(rooms, startDate, endDate) {
+  static availableRooms(rooms: Room[], startDate: string, endDate: string) {
     // Devuelve las habitaciones que no estan ocupadas en el rango pasado por parametros, incluidos los extremos. === HABITACIONES LIBRES ENTRE EL RANGO PASADO
     let occupiedList = [];
 
@@ -29,13 +41,13 @@ class Room {
     return occupiedList;
   }
 
-  isOccupied(date) {
+  isOccupied(date: string) {
     // Devuelve Falso si la hab. no esta ocupada = DISPONIBLE
     // Devuelve True si la hab. esta ocupada = RESERVADA
     for (const booking of this.bookings) {
-      const dateTimeBooking = date.getTime();
-      const startDateBooking = booking.checkIn.getTime();
-      const endDateBooking = booking.checkIn.getTime();
+      const dateTimeBooking = date;
+      const startDateBooking = booking.checkIn;
+      const endDateBooking = booking.checkOut;
 
       if (
         (dateTimeBooking >= startDateBooking && dateTimeBooking <= endDateBooking) ||
@@ -47,15 +59,15 @@ class Room {
     return false;
   }
 
-  occupancyPercentage(startDate, endDate) {
+  occupancyPercentage(startDate: string, endDate: string) {
     // Devuelve el Porcentage de ocupación dentro del rango pasado por parámetros, incluidos los extremos
     let availableAmount = 0;
 
     for (const booking of this.bookings) {
-      const startTime = startDate.getTime();
-      const endTime = endDate.getTime();
-      const startDateBooking = booking.checkin.getTime();
-      const endDateBooking = booking.checkout.getTime();
+      const startTime = startDate;
+      const endTime = endDate;
+      const startDateBooking = booking.checkIn;
+      const endDateBooking = booking.checkOut;
 
       if ((startTime >= startDateBooking && endTime <= endDateBooking) || startTime == startDateBooking || endTime == endDateBooking) {
         availableAmount += 1;
@@ -66,19 +78,35 @@ class Room {
   }
 }
 
+interface BookingInterface {
+  name: string;
+  email: string;
+  arrival: string;
+  departure: string;
+  discount: number;
+  room: Room;
+}
+
 class Booking {
-  constructor(name, email, checkIn, checkOut, discount, room) {
+  name: string;
+  email: string;
+  checkIn: string;
+  checkOut: string;
+  discount: number;
+  room: Room;
+
+  constructor({ name, email, arrival, departure, discount, room }: BookingInterface) {
     this.name = name;
     this.email = email;
-    this.checkIn = checkIn;
-    this.checkOut = checkOut;
+    this.checkIn = arrival;
+    this.checkOut = departure;
     this.discount = discount;
     this.room = room;
   }
 
   fee() {
     // Devuelve el precio final de la reserva contando el descuento de la habitacion (Hotel) y de la reserva (Pagina de reserva)
-    const roomFee = this.room.rate - this.room.rate * (this.room.discount / 100);
+    const roomFee = this.room.price - this.room.price * (this.room.discount / 100);
     return roomFee - roomFee * (this.discount / 100);
   }
 }
